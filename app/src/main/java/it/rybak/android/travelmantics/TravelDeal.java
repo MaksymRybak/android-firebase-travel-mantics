ce887078b8eb6b106fb74a42dd7b68a1d36ba61a
Maksym Rybak <rybak.maksym@gmail.com>
2020-09-12 14:50:16 +0200

    example of how we can read from firebase realtime database

---
+++ app/src/main/java/it/rybak/android/travelmantics/TravelDeal.java
@@ -0,0 +1,57 @@
+package it.rybak.android.travelmantics;
+
+public class TravelDeal {
+    private String id;
+    private String title;
+    private String description;
+    private String price;
+    private String image;
+
+    public TravelDeal(String title, String description, String price, String image) {
+        this.setId(id);
+        this.setTitle(title);
+        this.setDescription(description);
+        this.setPrice(price);
+        this.setImage(image);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
