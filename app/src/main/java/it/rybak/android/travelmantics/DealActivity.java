bb5bf20bf14873bd07266732d7eda4942347a3a0
Maksym Rybak <rybak.maksym@gmail.com>
2020-09-15 14:30:36 +0200

    add menu for edit end delete travel deal from firebase realtime db

--- app/src/main/java/it/rybak/android/travelmantics/DealActivity.java
+++ app/src/main/java/it/rybak/android/travelmantics/DealActivity.java
@@ -3,6 +3,7 @@ package it.rybak.android.travelmantics;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,6 +21,7 @@ public class DealActivity extends AppCompatActivity {
     EditText txtTitle;
     EditText txtDescription;
     EditText txtPrice;
+    TravelDeal deal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +35,18 @@ public class DealActivity extends AppCompatActivity {
         txtTitle = findViewById(R.id.txtTitle);
         txtDescription = findViewById(R.id.txtDescription);
         txtPrice = findViewById(R.id.txtPrice);
+
+        Intent intent = getIntent();
+        TravelDeal deal = (TravelDeal) intent.getSerializableExtra("Deal");
+        if (deal == null) {
+            // request to create new deal
+            deal = new TravelDeal();
+        }
+        this.deal = deal;
+
+        txtTitle.setText(deal.getTitle());
+        txtDescription.setText(deal.getDescription());
+        txtPrice.setText(deal.getPrice());
     }
 
     @Override
@@ -42,6 +56,13 @@ public class DealActivity extends AppCompatActivity {
                 saveDeal();
                 Toast.makeText(this, "Deal saved", Toast.LENGTH_LONG).show();
                 clean();
+                backToList();
+                return true;
+            }
+            case R.id.delete_menu: {
+                deleteDeal();
+                Toast.makeText(this, "Deal deleted", Toast.LENGTH_LONG).show();
+                backToList();
                 return true;
             }
             default:
@@ -57,11 +78,27 @@ public class DealActivity extends AppCompatActivity {
     }
 
     private void saveDeal() {
-        String title = txtTitle.getText().toString();
-        String description = txtDescription.getText().toString();
-        String price = txtPrice.getText().toString();
-        TravelDeal travelDeal = new TravelDeal(title, description, price, "");
-        databaseReference.push().setValue(travelDeal);
+        deal.setTitle(txtTitle.getText().toString());
+        deal.setDescription(txtDescription.getText().toString());
+        deal.setPrice(txtPrice.getText().toString());
+        if (deal.getId() == null) {
+            databaseReference.push().setValue(deal);
+        } else {
+            databaseReference.child(deal.getId()).setValue(deal);
+        }
+    }
+
+    private void deleteDeal() {
+        if (deal == null) {
+            Toast.makeText(this, "Please save the deal before deleting", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        databaseReference.child(deal.getId()).removeValue();
+    }
+
+    private void backToList() {
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
     }
 
     private void clean() {
--- app/src/main/java/it/rybak/android/travelmantics/DealAdapter.java
+++ app/src/main/java/it/rybak/android/travelmantics/DealAdapter.java
@@ -1,6 +1,7 @@
 package it.rybak.android.travelmantics;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -85,7 +86,7 @@ public class DealAdapter extends RecyclerView.Adapter<DealAdapter.DealViewHoler>
         return deals.size();
     }
 
-    public class DealViewHoler extends RecyclerView.ViewHolder {
+    public class DealViewHoler extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView tvTitle;
         TextView tvDescription;
         TextView tvPrice;
@@ -95,12 +96,24 @@ public class DealAdapter extends RecyclerView.Adapter<DealAdapter.DealViewHoler>
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvDescription = itemView.findViewById(R.id.tvDescription);
             tvPrice = itemView.findViewById(R.id.tvPrice);
+            itemView.setOnClickListener(this);
         }
 
         public void bind(TravelDeal travelDeal){
             tvTitle.setText(travelDeal.getTitle());
             tvDescription.setText(travelDeal.getDescription());
             tvPrice.setText(travelDeal.getPrice());
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            Log.d("Click", String.valueOf(position));
+
+            TravelDeal selectedDeal = deals.get(position);
+            Intent intent = new Intent(itemView.getContext(), DealActivity.class);
+            intent.putExtra("Deal", selectedDeal);
+            itemView.getContext().startActivity(intent);
         }
     }
 
